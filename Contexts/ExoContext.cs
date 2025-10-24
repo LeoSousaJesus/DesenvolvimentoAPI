7d3d4db392341491606f7cb8e47f6770ee0860c2
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-10-24 05:12:31 -0300

    Ajustando Context

--- Contexts/ExoContext.cs
+++ Contexts/ExoContext.cs
@@ -14,7 +14,14 @@ namespace Exo.WebApi.Contexts
         {
             if(!optionsBuilder.IsConfigured)
             {
+                // estabelece a string de conexão com o banco de dados SQL Server
                 optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;" + "Database=ExoApi;Trusted_Connection=True;");
+                
+                // Exemplo 2 de string de conexão para SQL Server com autenticação SQL
+                // optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;" + "Database=ExoApi;User Id=seu_usuario;Password=sua_senha;");
+
+                // Exemplo 3 de string de conexão para SQL Server com autenticação SQL
+                // User Id=sa;Password=admin;Server=localhost;Database=ExoApi; + Trusted_Connection=False;
             }
         }
         public DbSet<Projeto> Projetos { get; set; }
